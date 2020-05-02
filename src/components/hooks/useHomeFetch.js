ff83df53de677973b8cc32b2b7dcf54642c88c0d
Ilya Petrov <dnamix1@gmail.com>
2020-05-02 10:04:43 +0300

    data-fetching custom hook

---
+++ src/components/hooks/useHomeFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { API_URL, API_KEY } from "../../config";
+
+export const useHomeFetch = () => {
+  const [state, setState] = useState({ movies: [] });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchMovies = async (endpoint) => {
+    setError(false);
+    setLoading(true);
+    try {
+      const result = await (await fetch(endpoint)).json();
+      setState((prevState) => ({
+        ...prevState,
+        movies: [...result.results],
+        heroImage: prevState.heroImage || result.results[0],
+        currentPage: result.page,
+        totalPages: result.total_pages,
+      }));
+    } catch (err) {
+      setError(true);
+      console.log(err);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
+  }, []);
+
+  return [{ state, loading, error }, fetchMovies];
+};
