e71348a47d5f70a2596e7c0921fa5720b9f09833
Ilya Petrov <dnamix1@gmail.com>
2020-05-03 11:16:27 +0300

    header finished with styling

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
+import RMDBLogo from "../images/reactMovie_logo.png";
+import TMDBLogo from "../images/tmdb_logo.svg";
+import {
+  StyledHeader,
+  StyledRMDBLogo,
+  StyledTMDBLogo,
+} from "../styles/StyledHeader";
 
 const Header = () => {
   return (
-    <div>
-         Header   
-    </div>
-  )
-}
+    <StyledHeader>
+      <div className="header-content">
+        <StyledRMDBLogo src={RMDBLogo} alt="React Movie Logo" />
+        <StyledTMDBLogo src={TMDBLogo} alt="powered by image" />
+      </div>
+    </StyledHeader>
+  );
+};
 
 export default Header;
-
