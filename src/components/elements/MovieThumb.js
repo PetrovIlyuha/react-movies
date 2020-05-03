d461a31b38ee0b4bb561a766d2af2728dd1de092
Ilya Petrov <dnamix1@gmail.com>
2020-05-03 11:20:32 +0300

    movie Thumbnail card component

--- src/components/elements/MovieThumb.js
+++ src/components/elements/MovieThumb.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { StyledMovieThumb } from "../styles/StyledMovieThumb";
 
-const MovieThumb = () => {
-  return <div>Movie Thumb</div>;
+const MovieThumb = ({ image, movieId, clickable }) => {
+  return (
+    <StyledMovieThumb>
+      {clickable ? (
+        <img className="clickable" src={image} alt="movie image thumbnail" />
+      ) : (
+        <img src={image} alt="movie thumbnail" />
+      )}
+    </StyledMovieThumb>
+  );
 };
 
 export default MovieThumb;
