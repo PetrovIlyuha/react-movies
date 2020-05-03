01a088f03d9b9acdc114fd33ef7cdf5b3b1bebb3
Ilya Petrov <dnamix1@gmail.com>
2020-05-03 11:11:56 +0300

    global style wit styled-components

--- src/components/styles/GlobalStyle.js
+++ src/components/styles/GlobalStyle.js
@@ -1,9 +1,12 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: Lato, serif;
+    font-weight: 700;
+    font-size: 1.2rem;
   }
 `;
