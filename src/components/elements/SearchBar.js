554211276d42cf5b79d0f76f8e154444a15fce66
Ilya Petrov <dnamix1@gmail.com>
2020-05-04 11:27:43 +0300

    passing the search term from now controlled component of the search bar up to the Home page (local state was used with useState hook from react

--- src/components/elements/SearchBar.js
+++ src/components/elements/SearchBar.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import FontAwesome from "react-fontawesome";
 
-const SearchBar = () => {
-  return <div>Search</div>;
+import {
+  StyledSearchBar,
+  StyledSearchBarContent,
+} from "../styles/StyledSearchBar";
+
+const SearchBar = ({ callback }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const timeOut = useRef(null);
+
+  const handleMovieSearch = (e) => {
+    clearTimeout(timeOut.current);
+    setSearchTerm(e.target.value);
+    timeOut.current = setTimeout(() => {
+      callback(searchTerm);
+    }, 500);
+  };
+  return (
+    <StyledSearchBar>
+      <StyledSearchBarContent>
+        <FontAwesome className="fa-search" name="search" size="2x" />
+        <input
+          type="text"
+          placeholder="Search Movie..."
+          onChange={handleMovieSearch}
+          value={searchTerm}
+        />
+      </StyledSearchBarContent>
+    </StyledSearchBar>
+  );
 };
 
 export default SearchBar;
