1751c559a91966c26cb0a4fdd9419e7e06ed2f77
Ilya Petrov <dnamix1@gmail.com>
2020-05-08 15:50:51 +0300

    more prop type checking

--- src/components/elements/Grid.js
+++ src/components/elements/Grid.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StyledGrid, StyledGridContent } from "../styles/StyledGrid";
+import PropTypes from "prop-types";
 
 const Grid = ({ header, children }) => {
   return (
@@ -8,6 +9,10 @@ const Grid = ({ header, children }) => {
       <StyledGridContent>{children}</StyledGridContent>
     </StyledGrid>
   );
+};
+
+Grid.propTypes = {
+  header: PropTypes.string,
 };
 
 export default Grid;
--- src/components/elements/SingleMoviePage/MovieInfo.js
+++ src/components/elements/SingleMoviePage/MovieInfo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import NoImage from "../../images/no_image.jpg";
 import { IMAGE_BASE_URL, POSTER_SIZE } from "../../../config";
@@ -32,7 +33,7 @@ const MovieInfo = ({ movie }) => {
             <div className="director">
               <h3>DIRECTOR {movie.directors.length > 1 ? "S" : ""}</h3>
               {movie.directors.map((director) => (
-                <p key={movie.credit_id}>{director.name}</p>
+                <p key={movie.id}>{director.name}</p>
               ))}
             </div>
           </div>
@@ -40,6 +41,11 @@ const MovieInfo = ({ movie }) => {
       </div>
     </StyledMovieInfo>
   );
+};
+
+MovieInfo.propTypes = {
+  movie: PropTypes.object,
+  directors: PropTypes.array,
 };
 
 export default MovieInfo;
