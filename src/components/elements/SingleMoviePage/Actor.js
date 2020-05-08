30816dbabdfab4f6c80c9134d4181a8d0d2e5617
Ilya Petrov <dnamix1@gmail.com>
2020-05-08 15:50:04 +0300

    started to add prop types

--- src/components/elements/SingleMoviePage/Actor.js
+++ src/components/elements/SingleMoviePage/Actor.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import NoImage from "../../images/no_image.jpg";
 import { IMAGE_BASE_URL, POSTER_SIZE } from "../../../config";
@@ -20,3 +21,7 @@ const Actor = ({ actor }) => (
 );
 
 export default Actor;
+
+Actor.propTypes = {
+  actor: PropTypes.object,
+};
